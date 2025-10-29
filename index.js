42666b508aee6ec0febdfcdb11d0bf90dc03b4ef
Ashwin Kulangara Shaji <64102557+Astarioashwi@users.noreply.github.com>
2025-10-29 16:21:01 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+let isDOBOpen = false;
+
+const settingCogE1 = document.getElementById("settingIcon");
+const settingContentE1 = document.getElementById("settingContent");
+
+
+const toggleDataOfBirthSelector = () => {
+
+}
